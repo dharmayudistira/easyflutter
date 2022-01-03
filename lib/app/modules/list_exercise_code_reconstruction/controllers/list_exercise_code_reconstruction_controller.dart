eb3437b97d2ad03b73d1239686fe08fa859df415
dharmayudistira <dharmayudistira2000@gmail.com>
2022-01-03 14:18:20 +0700

    list exercise for code reconstruction

--- lib/app/modules/list_exercise_code_reconstruction/controllers/list_exercise_code_reconstruction_controller.dart
+++ lib/app/modules/list_exercise_code_reconstruction/controllers/list_exercise_code_reconstruction_controller.dart
@@ -1,20 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:easyflutter/app/data/class_model.dart';
+import 'package:easyflutter/app/data/exercise_model.dart';
+import 'package:easyflutter/app/utils/converter_helper.dart';
+import 'package:easyflutter/app/utils/storage_helper.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ListExerciseCodeReconstructionController extends GetxController {
-  //TODO: Implement ListExerciseCodeReconstructionController
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+  final _storageHelper = Get.find<StorageHelper>();
+  final exerciseReference = FirebaseFirestore.instance.collection("latihan");
+
+  var listExercise = <ExerciseModel>[];
+
+  Stream<QuerySnapshot> getAllExercise() {
+    final classId = _storageHelper.getIdClassUser();
+    return exerciseReference.where("id_kelas", isEqualTo: classId).snapshots();
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  void mapExerciseFirestoreToExerciseModel(AsyncSnapshot<QuerySnapshot> snapshots) {
+    final result = ConverterHelper.mapExerciseFirestoreToExerciseModelByType(snapshots, "code");
+    result.sort((a, b) => a.exerciseId!.compareTo(b.exerciseId!));
+    listExercise = result;
   }
 
-  @override
-  void onClose() {}
-  void increment() => count.value++;
+
 }
