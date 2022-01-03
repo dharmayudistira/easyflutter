0e1fb883dd22b144c61349cf55a3734b58aaa13e
dharmayudistira <dharmayudistira2000@gmail.com>
2022-01-03 10:22:20 +0700

    code dashboard student content

--- lib/app/modules/dashboard_student/views/dashboard_student_view.dart
+++ lib/app/modules/dashboard_student/views/dashboard_student_view.dart
@@ -19,7 +19,7 @@ class DashboardStudentView extends GetView<DashboardStudentController> {
           return IndexedStack(
             index: selectedIndex,
             children: [
-              _buildDashboardContent(),
+              _buildDashboardContent(context),
               ListExerciseCodeReconstructionView(),
               ListExerciseWidgetTreeReconstructionView(),
             ],
@@ -85,10 +85,135 @@ class DashboardStudentView extends GetView<DashboardStudentController> {
     );
   }
 
-  Widget _buildDashboardContent() {
+  Widget _buildDashboardContent(BuildContext context) {
     return SingleChildScrollView(
       child: Column(
-        children: [],
+        children: [
+          _buildCodeReconstructionInformation(context),
+          Container(color: Colors.black.withOpacity(0.1), height: dimenLarge),
+          _buildWidgetTreeReconstructionInformation(context),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildWidgetTreeReconstructionInformation(BuildContext context) {
+    return Container(
+      width: Get.size.width,
+      height: Get.size.height - AppBar().preferredSize.height,
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: dimenExtraLarge,
+          vertical: dimenMedium,
+        ),
+        child: Container(
+          child: Row(
+            children: [
+              Expanded(
+                flex: 4,
+                child: Container(
+                  child: Padding(
+                    padding: EdgeInsets.all(dimenSmall),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(dimenSmall),
+                      child: Image.asset(
+                        "assets/images/img_widget_tree_reconstruction.png",
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 3,
+                child: Container(
+                  child: Padding(
+                    padding: EdgeInsets.all(dimenMedium),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Apa itu\nWidget Tree Reconstruction ?",
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline4
+                              ?.copyWith(color: Colors.black),
+                        ),
+                        SizedBox(height: dimenMedium),
+                        Text(
+                          "Widget Tree Reconstruction menerapkan metode Concept Map. "
+                          "Mahasiswa diberikan peta konsep dari dosen (ahli) yang sudah "
+                          "dihilangkan beberapa bagian, sehingga mahasiswa perlu melengkapi peta "
+                          "konsep tersebut dengan jawaban yang sudah disediakan.",
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCodeReconstructionInformation(BuildContext context) {
+    return Container(
+      width: Get.size.width,
+      height: Get.size.height - AppBar().preferredSize.height,
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: dimenExtraLarge,
+          vertical: dimenMedium,
+        ),
+        child: Container(
+          child: Row(
+            children: [
+              Expanded(
+                flex: 3,
+                child: Container(
+                  child: Padding(
+                    padding: EdgeInsets.all(dimenMedium),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Apa itu\nCode Reconstruction ?",
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline4
+                              ?.copyWith(color: Colors.black),
+                        ),
+                        SizedBox(height: dimenMedium),
+                        Text(
+                          "Code Reconstruction menerapkan metode Parsons Problem, "
+                          "di mana terdapat blok-blok kode yang sudah disusun secara "
+                          "acak sehingga mahasiswa perlu menyusunnya ke susunan yang benar.",
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 4,
+                child: Container(
+                  child: Padding(
+                    padding: EdgeInsets.all(dimenSmall),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(dimenSmall),
+                      child: Image.asset(
+                        "assets/images/img_code_reconstruction.png",
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
