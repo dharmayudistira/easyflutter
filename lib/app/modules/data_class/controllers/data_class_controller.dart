f1156eaf7960421d6dd3351c7b41510539b41b8c
dharmayudistira <dharmayudistira2000@gmail.com>
2022-01-17 10:56:45 +0700

    fixing add exercise for code reconstruction

--- lib/app/modules/data_class/controllers/data_class_controller.dart
+++ lib/app/modules/data_class/controllers/data_class_controller.dart
@@ -13,18 +13,38 @@ class DataClassController extends GetxController {
   final classReference = FirebaseFirestore.instance.collection("kelas");
   final exerciseReference = FirebaseFirestore.instance.collection("latihan");
   final listTitleExerciseCodeReconstruction = [
-    "Latihan A",
-    "Latihan B",
-    "Latihan C",
-    "Latihan D",
-    "Latihan E",
+    "Latihan 1 : Belajar Column dengan MainAxis.Start + CrossAxis.End",
+    "Latihan 2 : Belajar Column dengan MainAxis.Center + CrossAxis.Start",
+    "Latihan 3 : Belajar Column dengan MainAxis.End + CrossAxis.Center",
+    "Latihan 4 : Belajar Row dengan MainAxis.Start + CrossAxis.End",
+    "Latihan 5 : Belajar Row dengan MainAxis.Center + CrossAxis.Start",
+    "Latihan 6 : Belajar Row dengan MainAxis.End + CrossAxis.Center",
+    "Latihan 7 : Belajar Column dengan MainAxis.SpaceBetween",
+    "Latihan 8 : Belajar Row dengan MainAxis.SpaceBetween",
+    "Latihan 9 : Belajar Column dengan MainAxis.SpaceAround",
+    "Latihan 10 : Belajar Row dengan MainAxis.SpaceAround",
+    "Latihan 11 : Belajar Column dengan MainAxis.SpaceEvenly",
+    "Latihan 12 : Belajar Row dengan MainAxis.SpaceEvenly",
+    "Latihan 13 : Belajar Column + Row",
+    "Latihan 14 : Belajar Column + Row dengan MainAxis",
+    "Latihan 15 : Belajar Column + Row dengan MainAxis + CrossAxis",
   ];
   final listTitleExerciseWidgetTreeReconstruction = [
-    "Latihan A",
-    "Latihan B",
-    "Latihan C",
-    "Latihan D",
-    "Latihan E",
+    "Latihan 1",
+    "Latihan 2",
+    "Latihan 3",
+    "Latihan 4",
+    "Latihan 5",
+    "Latihan 6",
+    "Latihan 7",
+    "Latihan 8",
+    "Latihan 9",
+    "Latihan 10",
+    "Latihan 11",
+    "Latihan 12",
+    "Latihan 13",
+    "Latihan 14",
+    "Latihan 15",
   ];
 
   late TextEditingController edtControllerClassName;
