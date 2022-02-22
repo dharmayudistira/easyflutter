86b745c25d2e92c13cfd7d336f32b220aeb36321
dharmayudistira <dharmayudistira2000@gmail.com>
2022-02-22 10:40:07 +0700

    fix generate exercise to util class

--- lib/app/modules/data_class/controllers/data_class_controller.dart
+++ lib/app/modules/data_class/controllers/data_class_controller.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:easyflutter/app/data/class_model.dart';
 import 'package:easyflutter/app/modules/dashboard_lecturer/controllers/dashboard_lecturer_controller.dart';
 import 'package:easyflutter/app/utils/converter_helper.dart';
+import 'package:easyflutter/app/utils/generate_exercises_helper.dart';
 import 'package:easyflutter/app/utils/snackbar_helper.dart';
 import 'package:easyflutter/app/utils/storage_helper.dart';
 import 'package:flutter/cupertino.dart';
@@ -13,40 +14,11 @@ class DataClassController extends GetxController {
   final GlobalKey<FormState> dataClassFormKey = GlobalKey();
   final classReference = FirebaseFirestore.instance.collection("kelas");
   final exerciseReference = FirebaseFirestore.instance.collection("latihan");
-  final listTitleExerciseCodeReconstruction = [
-    "Latihan 1 : Belajar Column dengan MainAxis.Start + CrossAxis.End",
-    "Latihan 2 : Belajar Column dengan MainAxis.Center + CrossAxis.Start",
-    "Latihan 3 : Belajar Column dengan MainAxis.End + CrossAxis.Center",
-    "Latihan 4 : Belajar Row dengan MainAxis.Start + CrossAxis.End",
-    "Latihan 5 : Belajar Row dengan MainAxis.Center + CrossAxis.Start",
-    "Latihan 6 : Belajar Row dengan MainAxis.End + CrossAxis.Center",
-    "Latihan 7 : Belajar Column dengan MainAxis.SpaceBetween",
-    "Latihan 8 : Belajar Row dengan MainAxis.SpaceBetween",
-    "Latihan 9 : Belajar Column dengan MainAxis.SpaceAround",
-    "Latihan 10 : Belajar Row dengan MainAxis.SpaceAround",
-    "Latihan 11 : Belajar Column dengan MainAxis.SpaceEvenly",
-    "Latihan 12 : Belajar Row dengan MainAxis.SpaceEvenly",
-    "Latihan 13 : Belajar Column + Row",
-    "Latihan 14 : Belajar Column + Row dengan MainAxis",
-    "Latihan 15 : Belajar Column + Row dengan MainAxis + CrossAxis",
-  ];
-  final listTitleExerciseWidgetTreeReconstruction = [
-    "Latihan 1",
-    "Latihan 2",
-    "Latihan 3",
-    "Latihan 4",
-    "Latihan 5",
-    "Latihan 6",
-    "Latihan 7",
-    "Latihan 8",
-    "Latihan 9",
-    "Latihan 10",
-    "Latihan 11",
-    "Latihan 12",
-    "Latihan 13",
-    "Latihan 14",
-    "Latihan 15",
-  ];
+  
+  final listTitleExerciseCodeReconstruction =
+      GenerateExercisesHelper.listExerciseCodeReconstruction;
+  final listTitleExerciseWidgetTreeReconstruction =
+      GenerateExercisesHelper.listExerciseWidgetTreeReconstruction;
 
   late TextEditingController edtControllerClassName;
 
